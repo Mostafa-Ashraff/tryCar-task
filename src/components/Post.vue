262ea0180560ea5566b749701b375335bff5cf5b
Mostafa-Ashraff <mostafaashrafwork@gmail.com>
2024-06-24 10:09:11 +0300

    basic functionality

---
+++ src/components/Post.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+defineProps({
+  title: String,
+  imgSrc: String,
+  userName: String,
+  phoneNumber: String,
+})
+
+</script>
+
+<template>
+  <h2>{{ userName }}{{ phoneNumber }}</h2>
+  <h4>{{ title }}</h4>
+  <img :src="imgSrc" alt="">
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
